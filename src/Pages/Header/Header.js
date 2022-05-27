e01dd16f1c7a9ad089933f292810986178af5225
pritom-roy <pritomroy20062@gmail.com>
2022-05-27 14:34:24 +0600

    header home

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -1,3 +1,4 @@
+import { Button } from "bootstrap";
 import React from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
@@ -6,6 +7,28 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="header">
+
+      <div className="row m-5">
+        <div className="col-lg-3 col-md-12 col-sm-12 ">
+         <div className="d-flex justify-content-center">
+         <h2 >PrintUp</h2>
+         </div>
+        </div>
+        <div className="col-lg-8">
+          <div className="input-top-header">
+          <input type='text' value='what do you need' className="ps-2"/>
+          <button className="btn mb-2 p-2 ms-1">Search</button>
+          </div>
+        </div>
+        <div className="col-lg-1 col-md-12 col-sm-12">
+         <div className="d-flex justify-content-center">
+         <button className="btn">login</button>
+         </div>
+        </div>
+
+
+      </div>
+
       <Navbar bg="black" expand="lg">
         <Container>
           <Navbar.Toggle aria-controls="basic-navbar-nav" className="toggle" />
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,48 @@
 import React from "react";
+import { Carousel } from "react-bootstrap";
+import "./Home.css"
 
 const Home = () => {
-  return <div></div>;
+  return <div>
+    <Carousel variant="dark" className="mt-1">
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://cdn.pixabay.com/photo/2016/10/26/19/12/design-1772284_960_720.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://cdn.pixabay.com/photo/2016/11/18/17/14/cloth-1835894_960_720.jpg"
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://cdn.pixabay.com/photo/2016/11/18/17/14/cloth-1835894_960_720.jpg"
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  </div>;
 };
 
 export default Home;
