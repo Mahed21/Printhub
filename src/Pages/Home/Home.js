bdd9a844e04e7128e85f1c5dd1cd5ac7de730034
Mahed21 <mahed255261@gmail.com>
2022-06-29 13:37:19 +0600

    footer

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,48 +1,50 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
-import "./Home.css"
+import "./Home.css";
+import shirt from "../../image/ti-shirt.png";
+import mug from "../../image/mug.png";
+import offer from "../../image/offer.png";
 
 const Home = () => {
-  return <div>
-    <Carousel variant="dark" className="mt-1">
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://cdn.pixabay.com/photo/2016/10/26/19/12/design-1772284_960_720.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://cdn.pixabay.com/photo/2016/11/18/17/14/cloth-1835894_960_720.jpg"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://cdn.pixabay.com/photo/2016/11/18/17/14/cloth-1835894_960_720.jpg"
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  </div>;
+  return (
+    <div className="mb-5">
+      <div
+        id="carouselExampleControls"
+        class="carousel slide"
+        data-bs-ride="carousel"
+      >
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={shirt} className="w-100 h-25 image" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={mug} class="d-block w-100 h-50 image" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={offer} class="d-block w-100 h-50 image" alt="..." />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleControls"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
