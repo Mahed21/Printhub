0665368d8bc11135c53b4c0009660b33f2869af7
Mahed21 <mahed255261@gmail.com>
2022-11-09 19:05:55 +0600

    deleted item

--- src/Pages/Firebase/Firebase.initial.js
+++ src/Pages/Firebase/Firebase.initial.js
@@ -7,12 +7,6 @@ const initializeAuthentication = () => {
     storageBucket: process.env.REACT_APP_storageBucket,
     messagingSenderId: process.env.REACT_APP_messagingSenderId,
     appId: process.env.REACT_APP_messagingSenderId,
-    // apiKey: "AIzaSyB3TE-7UT5ilK6u4uHaEGhxQm9oDG99wHY",
-    // authDomain: "printup-61eb6.firebaseapp.com",
-    // projectId: "printup-61eb6",
-    // storageBucket: "printup-61eb6.appspot.com",
-    // messagingSenderId: "968052814504",
-    // appId: "1:968052814504:web:f9b62185a055353f8cc89d",
   };
   const app = initializeApp(firebaseConfig);
 };
